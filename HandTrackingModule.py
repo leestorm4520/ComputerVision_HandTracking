f5963022f2729e95f95c898835a38a5916eae1f3
DuyLe <minhduy4520@gmail.com>
2022-06-15 22:26:16 -0500

    update

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,14 +4,15 @@ import mediapipe as mp
 
 
 class handDetector():
-    def __init__(self, static_image_mode=False, max_num_hands=2, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+    def __init__(self, static_image_mode=False, max_num_hands=2, complexity=1, min_detection_confidence=0.5, min_tracking_confidence=0.5):
         self.static_image_mode=static_image_mode
         self.max_num_hands=max_num_hands
+        self.complexity=complexity
         self.min_detection_confidence=min_detection_confidence
         self.min_tracking_confidence=min_tracking_confidence
         #find the hands on the camera
         self.mpHands=mp.solutions.hands
-        self.hands=self.mpHands.Hands(self.static_image_mode, self.min_detection_confidence, self.min_tracking_confidence )
+        self.hands=self.mpHands.Hands(self.static_image_mode, self.max_num_hands, self.complexity, self.min_detection_confidence, self.min_tracking_confidence )
         self.mpDraw=mp.solutions.drawing_utils
     def findHands(self, img, draw=True):
         imgRGB= cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
